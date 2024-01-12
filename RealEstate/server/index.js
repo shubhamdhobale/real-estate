e159cca35d914d2afdeb5c5d9c3718585cfd6f94
Shubham Dhobakle <97725662+shubhamdhobale@users.noreply.github.com>
2024-01-12 23:13:10 +0530

    create signup api route

--- RealEstate/server/index.js
+++ RealEstate/server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js'
 dotenv.config();
 
 const app = express();
@@ -12,7 +13,9 @@ mongoose.connect(process.env.MONGO).then(() => {
   console.error(err);
 })
 
+app.use(express.json());
 app.use('/server/user' , userRouter);
+app.use('/server/auth' , authRouter);
 
 app.listen(3000 , () => {
   console.log("Server is running on port 3000");
