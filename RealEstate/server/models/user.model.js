aeebcf1045268dfbce0877c16fdb59fe5cb6eb84
Shubham Dhobakle <97725662+shubhamdhobale@users.noreply.github.com>
2024-01-12 20:41:58 +0530

    create user model

---
+++ RealEstate/server/models/user.model.js
@@ -0,0 +1,23 @@
+import mongoose, { Mongoose } from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+  }
+}, { timestamps:true });
+
+
+const user = mongoose.model('User' , userSchema);
+
+export default User;
