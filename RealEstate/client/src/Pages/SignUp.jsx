23d2274620dcfa58b3beceefef114a2daaad7e99
Shubham Dhobakle <97725662+shubhamdhobale@users.noreply.github.com>
2024-01-14 10:34:02 +0530

    create signin api route

--- RealEstate/client/src/Pages/SignUp.jsx
+++ RealEstate/client/src/Pages/SignUp.jsx
@@ -1,74 +1,85 @@
-import { useState } from "react";
-import { Link , useNavigate } from "react-router-dom";
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+// import OAuth from '../components/OAuth';
 
-const SignUp = () => {
-const [formData , setformData] = useState({});
-const [error , setError] = useState(null);
-const [loading , setloading ] = useState(false);
-const navigate = useNavigate();
-  const changeHandler = (e) =>{
-    setformData({
+export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({
       ...formData,
-     [e.target.id] : e.target.value,
-});
-};
-
-const submitHandler = async (e) =>{
-  e.preventDefault();
-
-try {
-  
-  setloading(true);
-  
-    const res = await fetch('/server/auth/signup' , 
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(formData),
+      [e.target.id]: e.target.value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch('/server/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate('/signin');
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
     }
-    );
-    const data = await res.json();
-    console.log(data);
-    if(data.sucess === false){
-      setError(data.message),
-    setloading(false);
-    return;
-    }
-    setloading(false)
-    setError(null);
-   navigate('/signin');
-} catch (error) {
-  setloading(false);
-  setError(error.message);
-}
-
-}
-
-
+  };
   return (
-    <div className="p-3 max-w-lg mx-auto">
-      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-      <form action="" className="flex flex-col gap-4" onSubmit={submitHandler} >
-        <input type="text" placeholder="username" className="border p-3 rounded-lg" id="username" onChange={changeHandler} />
-        <input type="email" placeholder="email" className="border p-3 rounded-lg" id="email" onChange={changeHandler} />
-        <input type="password" placeholder="password" className="border p-3 rounded-lg" id="password" onChange={changeHandler} />
-        <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80 "> 
-        { loading ? 'loading...' : 'Sign up' }
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input
+          type='text'
+          placeholder='username'
+          className='border p-3 rounded-lg'
+          id='username'
+          onChange={handleChange}
+        />
+        <input
+          type='email'
+          placeholder='email'
+          className='border p-3 rounded-lg'
+          id='email'
+          onChange={handleChange}
+        />
+        <input
+          type='password'
+          placeholder='password'
+          className='border p-3 rounded-lg'
+          id='password'
+          onChange={handleChange}
+        />
+
+        <button
+          disabled={loading}
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+        >
+          {loading ? 'Loading...' : 'Sign Up'}
         </button>
+        {/* <OAuth/> */}
       </form>
-      <div className="flex gap-2 mt-5">
+      <div className='flex gap-2 mt-5'>
         <p>Have an account?</p>
-        <Link to={"/signin"}>
-          <span className="text-blue-700 hover:underline">SignIn</span>
+        <Link to={'/sign-in'}>
+          <span className='text-blue-700'>Sign in</span>
         </Link>
       </div>
-      { error && <p className="text-red-500 ">{error}</p> }
-
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
-    
-  )
+  );
 }
-
-export default SignUp
