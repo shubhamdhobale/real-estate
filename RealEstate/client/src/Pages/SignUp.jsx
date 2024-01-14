d0b9f45de39ec4b11c500095f411b8955e0c889e
Shubham Dhobakle <97725662+shubhamdhobale@users.noreply.github.com>
2024-01-14 10:49:16 +0530

    sign in page functionality

--- RealEstate/client/src/Pages/SignUp.jsx
+++ RealEstate/client/src/Pages/SignUp.jsx
@@ -75,7 +75,7 @@ export default function SignUp() {
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Have an account?</p>
-        <Link to={'/sign-in'}>
+        <Link to={'/signin'}>
           <span className='text-blue-700'>Sign in</span>
         </Link>
       </div>
