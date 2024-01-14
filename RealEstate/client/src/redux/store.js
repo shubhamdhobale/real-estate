f58890ee71116c9b6d1e3776f024b4641050987a
Shubham Dhobakle <97725662+shubhamdhobale@users.noreply.github.com>
2024-01-14 12:05:27 +0530

    adding redux persist

--- RealEstate/client/src/redux/store.js
+++ RealEstate/client/src/redux/store.js
@@ -1,11 +1,26 @@
-import { configureStore } from '@reduxjs/toolkit';
-import userReducer from './user/userSlice'
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import userReducer from './user/userSlice';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import persistStore from 'redux-persist/es/persistStore';
+
+
+const rootReducer = combineReducers({user : userReducer})
+
+const persistconfig = {
+  key : 'root',
+  storage,
+  version :1,
+}
+
+const persistedReducer = persistReducer(persistconfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {user: userReducer },
+  reducer: persistedReducer,
   middleware :(getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck:false,
     }),
 })
 
+export const persistor = persistStore(store);
