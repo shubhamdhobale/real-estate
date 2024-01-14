2be8f92c9af518b9245e55dee3a059a847d135e8
Shubham Dhobakle <97725662+shubhamdhobale@users.noreply.github.com>
2024-01-14 11:51:47 +0530

    adding redux toolkit

--- RealEstate/client/src/main.jsx
+++ RealEstate/client/src/main.jsx
@@ -2,8 +2,11 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { store } from '../src/redux/store.js';
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
-      <App />
+      <Provider store={ store }>
+            <App />
+      </Provider>
  )
