1b0caea552b4151ea586635163b9bab916c285a2
Shubham Dhobakle <97725662+shubhamdhobale@users.noreply.github.com>
2024-01-18 23:49:48 +0530

    profile Page UI

--- RealEstate/client/src/Pages/Profile.jsx
+++ RealEstate/client/src/Pages/Profile.jsx
@@ -1,8 +1,23 @@
-// import React from 'react'
+import { useSelector } from "react-redux"
 
 const Profile = () => {
+
+  const { currentUser } = useSelector((state) => state.user)
   return (
-    <div>Profile</div>
+    <div className="p-3 mx-auto max-w-lg">
+         <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+         <form action="" className=" flex flex-col gap-4">
+          <img src={currentUser.avatar} alt="profile" className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2" />
+          <input type="text" id="username" placeholder="username.." className="border p-3 rounded-lg"/>
+          <input type="text" id="email" placeholder="email.." className="border p-3 rounded-lg"/>
+          <input type="text" id="password" placeholder="password.." className="border p-3 rounded-lg"/>
+          <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">Update</button>
+          </form>
+          <div className="flex justify-between mt-5 ">
+            <span className="text-red-700 cursor-pointer">Delete account</span>
+            <span className="text-red-700 cursor-pointer">Sign Out</span>
+          </div>
+         </div>
   )
 }
 
